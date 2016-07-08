babdf4f594e65801e2d4faa45b4f5b66acfc50e2
Rob Winch <rwinch@gopivotal.com>
2016-07-08 16:21:54 -0500

    Use Spring Security 4.2.0.BUILD-SNAPSHOT

--- samples/httpsession-redis-json/src/main/java/sample/config/SessionConfig.java
+++ samples/httpsession-redis-json/src/main/java/sample/config/SessionConfig.java
@@ -1,6 +1,7 @@
 package sample.config;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -12,17 +13,18 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.jackson2.*;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.csrf.DefaultCsrfToken;
+import org.springframework.security.web.jackson2.*;
 import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import sample.mixins.*;
 
 import javax.servlet.http.Cookie;
 import java.util.Collections;
 
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.*;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.*;
 
 /**
  * @author jitendra on 3/3/16.
@@ -30,45 +32,67 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_
 @EnableRedisHttpSession
 public class SessionConfig {
 
-    @Bean
-    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-        return new GenericJackson2JsonRedisSerializer(objectMapper());
-    }
+	@Bean
+	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+		return new GenericJackson2JsonRedisSerializer(objectMapper());
+	}
 
-    @Bean
-    public JedisConnectionFactory connectionFactory() {
-        return new JedisConnectionFactory();
-    }
+	@Bean
+	public JedisConnectionFactory connectionFactory() {
+		return new JedisConnectionFactory();
+	}
 
-    /**
-     * Customized {@link ObjectMapper} to add mix-in for class that doesn't have default constructors
-     *
-     * @return
-     */
-    ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.setVisibility(
-                mapper.getVisibilityChecker()
-                        .withFieldVisibility(ANY)
-                        .withSetterVisibility(PUBLIC_ONLY)
-                        .withGetterVisibility(PUBLIC_ONLY)
-                        .withIsGetterVisibility(PUBLIC_ONLY)
-        );
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-        mapper.addMixIn(DefaultCsrfToken.class, DefaultCsrfTokenMixin.class);
-        mapper.addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityMixin.class);
-        mapper.addMixIn(BadCredentialsException.class, BadCredentialsExceptionMixin.class);
-        mapper.addMixIn(Collections.unmodifiableSet(Collections.EMPTY_SET).getClass(), UnmodifiableSetMixin.class);
+	/**
+	 * Customized {@link ObjectMapper} to add mix-in for class that doesn't have
+	 * default constructors
+	 *
+	 * @return
+	 */
+	ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper()
+				.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+		mapper.setVisibility(
+				mapper.getVisibilityChecker()
+						.withFieldVisibility(ANY)
+						.withGetterVisibility(ANY)
+		);
 
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(DefaultSavedRequest.class, new DefaultSavedRequestDeserializer(DefaultSavedRequest.class));
-        module.addDeserializer(User.class, new UserDeserializer());
-        module.addDeserializer(WebAuthenticationDetails.class, new WebAuthenticationDetailsDeserializer());
-        module.addDeserializer(Cookie.class, new HttpCookieDeserializer());
-        module.addDeserializer(UsernamePasswordAuthenticationToken.class, new UsernamePasswordAuthenticationTokenDeserializer());
-
-        mapper.registerModule(module);
-        return mapper;
-    }
+		mapper.addMixIn(UsernamePasswordAuthenticationToken.class, UsernamePasswordAuthenticationTokenMixin.class);
+		mapper.addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityMixin.class);
+		mapper.addMixIn(Cookie.class, CookieMixin.class);
+		mapper.addMixIn(DefaultSavedRequest.class, DefaultSavedRequestMixin.class);
+		return mapper;
+		// ObjectMapper mapper = new ObjectMapper();
+		// mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+		// false);
+		// mapper.setVisibility(
+		// mapper.getVisibilityChecker()
+		// .withFieldVisibility(ANY)
+		// .withSetterVisibility(PUBLIC_ONLY)
+		// .withGetterVisibility(PUBLIC_ONLY)
+		// .withIsGetterVisibility(PUBLIC_ONLY)
+		// );
+		// mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL,
+		// JsonTypeInfo.As.PROPERTY);
+		// mapper.addMixIn(DefaultCsrfToken.class, DefaultCsrfTokenMixin.class);
+		// mapper.addMixIn(SimpleGrantedAuthority.class,
+		// SimpleGrantedAuthorityMixin.class);
+		//// mapper.addMixIn(BadCredentialsException.class,
+		// BadCredentialsExceptionMixin.class);
+		// mapper.addMixIn(Collections.unmodifiableSet(Collections.EMPTY_SET).getClass(),
+		// UnmodifiableSetMixin.class);
+		//
+		// SimpleModule module = new SimpleModule();
+		//// module.addDeserializer(DefaultSavedRequest.class, new
+		// DefaultSavedRequestDeserializer(DefaultSavedRequest.class));
+		// module.addDeserializer(User.class, new UserDeserializer());
+		//// module.addDeserializer(WebAuthenticationDetails.class, new
+		// WebAuthenticationDetailsDeserializer());
+		//// module.addDeserializer(Cookie.class, new HttpCookieDeserializer());
+		// module.addDeserializer(UsernamePasswordAuthenticationToken.class, new
+		// UsernamePasswordAuthenticationTokenDeserializer());
+		//
+		// mapper.registerModule(module);
+		// return mapper;
+	}
 }
